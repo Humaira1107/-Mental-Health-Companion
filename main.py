811cf0299abf7604d9b185494e448a7f7ede0d58
Humaira1107 <humairaghafoor07@gmail.com>
2025-07-24 09:34:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -182,4 +182,5 @@ with gr.Blocks(css=custom_css) as demo:
 
     btn.click(fn=wrapper, inputs=[inp, sound], outputs=out)
 
-demo.launch()(server_name="0.0.0.0", server_port=8080)
+demo.launch(server_name="0.0.0.0", server_port=8080)
+
